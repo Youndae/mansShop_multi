3357a5efa99faa980782c8bc644d5d8a6ecff747
Youndae <namyoundae@gmail.com>
2025-12-06 07:19:24 +0900

    backend deploy setup

--- module-config/src/main/java/com/example/moduleconfig/properties/RedisProperties.java
+++ module-config/src/main/java/com/example/moduleconfig/properties/RedisProperties.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "spring.data.redis")
+@ConfigurationProperties(prefix = "spring.redis")
 @Getter
 @Setter
 public class RedisProperties {
