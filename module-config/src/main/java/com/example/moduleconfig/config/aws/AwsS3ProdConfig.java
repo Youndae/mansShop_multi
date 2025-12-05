771551627a26f6fb3a436bdf7b4bfd0380108b1d
Youndae <namyoundae@gmail.com>
2025-12-06 04:52:03 +0900

    backend deploy setup

---
+++ module-config/src/main/java/com/example/moduleconfig/config/aws/AwsS3ProdConfig.java
@@ -0,0 +1,23 @@
+package com.example.moduleconfig.config.aws;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("prod")
+public class AwsS3ProdConfig {
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public AmazonS3 amazonS3() {
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .build();
+    }
+}
